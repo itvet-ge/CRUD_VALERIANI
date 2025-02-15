698d42dfd5f767a9d64e38e3ffe4f41cdb8054ac
valeriani70 <kverna74@hotmail.com>
2025-02-15 10:39:43 +0400

    გასწორებული ვერსია

--- src/main/java/ge/itvet/homework/SpringBootHomework2024Application.java
+++ src/main/java/ge/itvet/homework/SpringBootHomework2024Application.java
@@ -3,7 +3,9 @@ package ge.itvet.homework;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class SpringBootHomework2024Application {
 
     public static void main(String[] args) {
---
+++ src/main/java/ge/itvet/homework/company/controller/CompanyController.java
@@ -0,0 +1,60 @@
+package ge.itvet.homework.company.controller;
+
+import ge.itvet.homework.company.model.Company;
+import ge.itvet.homework.company.service.CompanyService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/companies")
+
+public class CompanyController {
+
+    @Autowired
+    private CompanyService service;
+
+    @GetMapping("/{id}")
+    @Operation(description = "კომპანიის წამოღება ID-ით")
+    public ResponseEntity<Company> getCompanyById(@PathVariable Long id) {
+        Optional<Company> company = service.findById(id);
+        return company.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping
+    @Operation(description = "ყველა კომპანიის წამოღება (პეიჯინგით)")
+    public ResponseEntity<List<Company>> getAllCompanies(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        List<Company> companies = service.findAll(page, size);
+        return ResponseEntity.ok(companies);
+    }
+
+    @PostMapping
+    @Operation(description = "ახალი კომპანიის შექმნა")
+    public ResponseEntity<Company> createCompany(@RequestBody Company company) {
+        Company savedCompany = service.save(company);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCompany);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(description = "კომპანიის რედაქტირება")
+    public ResponseEntity<Company> updateCompany(@PathVariable Long id, @RequestBody Company company) {
+        company.setId(id);
+        Company updatedCompany = service.save(company);
+        return ResponseEntity.ok(updatedCompany);
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(description = "კომპანიის წაშლა")
+    public ResponseEntity<Void> deleteCompany(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
